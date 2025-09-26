4371fdd811775aede51a1b2f0bd7dd6607e96cc6
Steve Minor <stv.mnr@gmail.com>
2025-09-26 09:24:36 -0700

    initial commit

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from web import explorer, creature
+
+
+app = FastAPI()
+
+app.include_router(explorer.router)
+app.include_router(creature.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
