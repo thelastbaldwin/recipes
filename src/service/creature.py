67172bc51e52751dd02243cfb077384e29014af7
Steve Minor <stv.mnr@gmail.com>
2025-09-26 09:47:16 -0700

    tests

--- src/service/creature.py
+++ src/service/creature.py
@@ -5,7 +5,7 @@ def get_all() -> list[Creature]:
     return data.get_all()
 
 def get_one(name: str) -> Creature | None:
-    return data.get(id)
+    return data.get_one(name)
 
 def create(creature: Creature) -> Creature:
     return data.create(creature)
--- src/service/explorer.py
+++ src/service/explorer.py
@@ -5,7 +5,7 @@ def get_all() -> list[Explorer]:
     return data.get_all()
 
 def get_one(name: str) -> Explorer | None:
-    return data.get(name)
+    return data.get_one(name)
 
 def create(explorer: Explorer) -> Explorer:
     return data.create(explorer)
